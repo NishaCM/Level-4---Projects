f2396b626c44f5fc1915681b003e73afe2b133b1
Nisha <nishamohnish@gmail.com>
2022-04-20 21:14:58 +1200

    Portfolio update

---
+++ TravelWeb/GMJS.js
@@ -0,0 +1,31 @@
+//Event + Function
+function mOver(obj){
+    obj.innerHTML = "On Specials!!!"
+}
+   
+function mOut ( obj){
+    obj.innerHTML = "Get $250 off!!!";
+}
+
+//Get element by id - not sure if this worked
+
+function para(){
+    document.getElementById("para").style.fontSize = "60px";
+};
+
+
+//Use or Var , Let or Const
+function myFunction() {
+    let text;
+    let person = prompt("Please enter your name:", "First & Last");
+    if (person == null || person == "") {
+      text = "Hello! how can we help ?";
+    } else {
+      text = "Welcome"+ " " + person + " " +"! How are you today?";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
+    
+
+
+ 
